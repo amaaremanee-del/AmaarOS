41d82a3b7ed65f34ba8f388702bec4be1a7b9210
amaaremanee-del <amaar.emanee@education.nsw.gov.au>
2026-09-21 14:56:49 +1000

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,17 +1,34 @@
-// Firebase v9 modular setup for AmaarOS
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import {
+    getAuth,
+    setPersistence,
+    browserLocalPersistence,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3bQuCVkhpaL1qXp6MxEfsLTGJOosAWgQ",
-  authDomain: "amaaros-114a8.firebaseapp.com",
-  projectId: "amaaros-114a8",
-  storageBucket: "amaaros-114a8.appspot.com",
-  messagingSenderId: "988886953876",
-  appId: "1:988886953876:web:4d33951596402c2f83a981",
-  measurementId: "G-H99Z2B0L74"
+    apiKey: "AIzaSyC3bQuCVkhpaL1qXp6MxEfsLTGJOosAWgQ",
+    authDomain: "amaaros-114a8.firebaseapp.com",
+    projectId: "amaaros-114a8",
+    storageBucket: "amaaros-114a8.appspot.com",
+    messagingSenderId: "988886953876",
+    appId: "1:988886953876:web:4d33951596402c2f83a981",
+    measurementId: "G-H99Z2B0L74"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = getAuth(app);
+
+// ⭐ FORCE Firebase to save login session
+setPersistence(auth, browserLocalPersistence);
+
+export {
+    auth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut
+};
